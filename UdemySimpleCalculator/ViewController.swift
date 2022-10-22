39f7c0f1238c1c758d2344b464d5e53b08f32247
allpdev <alpkctpdev@gmail.com>
2022-10-22 22:40:51 +0300

    Connections

--- UdemySimpleCalculator/ViewController.swift
+++ UdemySimpleCalculator/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sumButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func minusButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func multiplyButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func divideButton(_ sender: Any) {
+    }
+    
 }
 
